b6a14819a8a4919c006d168ac87b2d3b51ff226a
Pierre Lardet <lardet.pierre@gmail.com>
2020-01-08 19:35:15 +0000

    added carousel

--- public/Javascript/javascript.js
+++ public/Javascript/javascript.js
@@ -1,35 +1,19 @@
 $(document).ready(function() {
 	if ($(window).width() < 768) {
-        var new_src = $("img[src$='obg1.jpg']").attr("src").replace('obg1.jpg', 'obg2.jpg'); 
-        $("img[src$='obg1.jpg']").attr("src", new_src); 
+        var new_src1 = $("img[src$='slide1.png']").attr("src").replace('slide1.png', 'slide1-2.png'); 
+        var new_src2 = $("img[src$='slide2.png']").attr("src").replace('slide2.png', 'slide2-2.png'); 
+        var new_src3 = $("img[src$='slide3.png']").attr("src").replace('slide3.png', 'slide3-2.png'); 
+        $("img[src$='slide1.png']").attr("src", new_src1); 
+        $("img[src$='slide2.png']").attr("src", new_src2);
+        $("img[src$='slide3.png']").attr("src", new_src3);
     };
+    var carouselHeight = window.innerHeight - 20;
+    $("img[class$='img-slide']").height(carouselHeight);
 });
 //changes the background image appropriately when the page is loaded depending on the size of screen.
 
-$(document).ready(function() {
-    $('#CREATEACCOUNTFORM').toggle();
-	$('.trigger-link').click(function(){
-	   $('#CREATEACCOUNTFORM').slideToggle(500);
-	   $('#LOGINFORM').slideToggle(500);
-	});
-});
-//toggles between create account and login forms on the account page.
 
 function animateDropdownNav(x) {
     x.classList.toggle("nav-icon-changed");
 }
 //trigger for the menu X animation to play.
-
-function arrowIconToWhite() {
-	document.getElementById("dropdown-arrow").src="IMAGES/dropdown-arrow-white.png";
-}
-function arrowIconToBlack() {
-	document.getElementById("dropdown-arrow").src="IMAGES/dropdown-arrow.png";
-};
-//toggles between white and black arrows when mouse is over search filters button.
-function downloadIconToWhite() {
-  document.getElementById("download-icon").src="IMAGES/download-arrow-white.png";
-}
-function downloadIconToBlue() {
-  document.getElementById("download-icon").src="IMAGES/download-arrow-blue.png";
-};
